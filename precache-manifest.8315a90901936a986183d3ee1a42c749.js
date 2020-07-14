6d9c33c564c9bc23ea1d08e3447045d498a484d3
rameshd100 <rameshdahal2013@gmail.com>
2020-07-13 20:30:38 -0400

    Updates

---
+++ precache-manifest.8315a90901936a986183d3ee1a42c749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09416cc190da2c22e71138bcbc7e2cb7",
+    "url": "/cool-robots/index.html"
+  },
+  {
+    "revision": "0545a5d96cff3d78f002",
+    "url": "/cool-robots/static/css/main.cc2f51bc.chunk.css"
+  },
+  {
+    "revision": "96baf6a2f269d80db703",
+    "url": "/cool-robots/static/js/2.54aac455.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cool-robots/static/js/2.54aac455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0545a5d96cff3d78f002",
+    "url": "/cool-robots/static/js/main.cd7e2787.chunk.js"
+  },
+  {
+    "revision": "9394237512cef87760ae",
+    "url": "/cool-robots/static/js/runtime-main.b40d2a73.js"
+  }
+]);
